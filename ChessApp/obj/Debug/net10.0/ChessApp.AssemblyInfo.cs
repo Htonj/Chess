3dba8febf667078ebd4d96efe13239ee76140aac
Phloam <cosmopolitesss34@gmail.com>
2026-03-24 20:30:22 +0300

    pieces nw can move

--- ChessApp/obj/Debug/net10.0/ChessApp.AssemblyInfo.cs
+++ ChessApp/obj/Debug/net10.0/ChessApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChessApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71f772b7eeee9643e47367af981a37473277f235")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChessApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChessApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
